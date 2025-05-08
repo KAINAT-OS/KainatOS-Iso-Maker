a4bc69f42215ed25566d4279e8c4e79ff7728134
kainatquaderee <68397736+kainatquaderee@users.noreply.github.com>
2025-05-08 21:10:32 +0600

    Update setup-chrrot.sh

--- setup-chrrot.sh
+++ setup-chrrot.sh
@@ -6,7 +6,7 @@ apt-get update && apt-get  install wget -y
 PREFS_FILE="/etc/apt/preferences.d/snapshot.pref"
 working_dir=$(mktemp -d)
 cd $working_dir
-wget https://github.com/KAINAT-OS/Kainatos-packages/raw/refs/heads/ppa/debs/kainat-os-sources-25.1.deb
+curl -L -O https://github.com/KAINAT-OS/Kainatos-packages/raw/refs/heads/ppa/debs/kainat-os-sources-25.1.deb
 dpkg -i ./*.deb
 
 rm -fv /etc/apt/sources.list.d/thorium.list && \
