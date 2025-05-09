ba123ffc714be0cf82cb2219c4f369e7c85a8f4b
kainatquaderee <68397736+kainatquaderee@users.noreply.github.com>
2025-05-09 14:03:56 +0600

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,11 +1,2 @@
 #!/bin/bash
-set -e
-source ./config.sh
-if [ -e "./Debian.iso" ]; then
-    echo "found it"
-else
-    wget -O ./Debian.iso https://cdimage.debian.org/mirror/cdimage/archive/$deb_version-live/amd64/iso-hybrid/debian-live-$deb_version-amd64-kde.iso
-    mkdir chroot
-fi
-cubic ./chroot ./Debian.iso
-return 0
+export deb_version=6.12.0
