12d9af996cfd187fe527df45043a02339de47de2
kainatquaderee <68397736+kainatquaderee@users.noreply.github.com>
2025-05-09 14:17:14 +0600

    Update setup-env.sh

--- setup-env.sh
+++ setup-env.sh
@@ -3,12 +3,15 @@ set -e
 source ./config.sh
 if [ -e "./Debian.iso" ]; then
     echo "found it"
+    
 else
     wget -O ./Debian.iso https://cdimage.debian.org/mirror/cdimage/archive/$deb_version-live/amd64/iso-hybrid/debian-live-$deb_version-amd64-kde.iso
-    if [ -d ./chroot]; then
-        mkdir chroot
-    fi
 fi
+
+if [ -d ./chroot]; then
+    mkdir chroot
+fi
+
 cp ./debian-to-KOS ./chroot/custom-root/bin/
 chmod +x ./chroot/custom-root/bin/debian-to-KOS
 cubic ./chroot ./Debian.iso
